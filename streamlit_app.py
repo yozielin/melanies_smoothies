c595e8500ca3016092d34d2d34419e5b60244934
yozielin <31376522+yozielin@users.noreply.github.com>
2025-03-20 19:36:23 +0000

    added containder form

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,4 +52,4 @@ if ingridients_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered, ' + name_on_order + '!' , icon="✅")
 
-st.text(smoothiefroot_response.json())
+sf_dt =  st.dataframe(st.text(smoothiefroot_response.json()), use_container_width=True)
