59b35d9523e593791d4f857d361aed906e26d6e4
yozielin <31376522+yozielin@users.noreply.github.com>
2025-03-20 20:00:36 +0000

    added pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,11 @@ cnx = st.connection("snowflake")
 session = cnx.session()
 # session = get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
-st.dataframe(data=my_dataframe, use_container_width=True)
+# st.dataframe(data=my_dataframe, use_container_width=True)
+# st.stop()
+
+pd_df=my_dataframe.to_pandas()
+st.dataframe(pd_df)
 st.stop()
 
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
