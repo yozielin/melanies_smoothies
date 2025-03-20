87c9bbefd40d0a94cd75c85182bbc636536d008f
yozielin <31376522+yozielin@users.noreply.github.com>
2025-03-20 13:57:50 +0100

    Update streamlit_app.py
    Connection chanaged

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
+# from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -9,8 +9,9 @@ st.write("Replace the code in this example app with your own code! And if you're
 
 
 
-
-session = get_active_session()
+cnx = st.connection("snowflake")
+session = cnx.session()
+# session = get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 # st.dataframe(data=my_dataframe, use_container_width=True)
 
