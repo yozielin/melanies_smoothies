f3a18b68ddd2cc1e00fe4eadd101ed83436127ab
yozielin <31376522+yozielin@users.noreply.github.com>
2025-03-20 20:04:29 +0000

    handel pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,8 +19,8 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 # st.stop()
 
 pd_df=my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+# st.dataframe(pd_df)
+# st.stop()
 
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 st.text(smoothiefroot_response)
@@ -41,6 +41,9 @@ if ingridients_list:
 
     for fruit_chosen in ingridients_list:
         ingredients_string += fruit_chosen + ' '
+
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search valuer for', fruit_chosen, ' is ', search_on, '.' )
         st.subheader(fruit_chosen + 'Nutrition Information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
         sf_dt =  st.dataframe(data = smoothiefroot_response.json(), use_container_width=True)
